73a6d0e710d0d28c305d064c7da46bf220e630b9
Piotr Sokołowski <dev@sokoloowski.pl>
2021-09-04 19:12:23 +0200

    Fixtures

---
+++ src/DataFixtures/SnippetFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Language;
+use App\Entity\Snippet;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class SnippetFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $language = new Language('C');
+        $snippet = new Snippet();
+        $snippet->setLanguage($language);
+        $snippet->setName('helloworld');
+        $snippet->setCode(
+            '#include <stdio.h>' . PHP_EOL .
+            'int main() {' . PHP_EOL .
+            '    printf("Hello, world!\n")' . PHP_EOL .
+            '    return 0;' . PHP_EOL .
+            '}'
+        );
+        $manager->persist($language);
+        $manager->persist($snippet);
+
+        $language = new Language('C++');
+        $snippet = new Snippet();
+        $snippet->setLanguage($language);
+        $snippet->setName('helloworld');
+        $snippet->setCode(
+            '#include <iostream>' . PHP_EOL .
+            'using namespace std;' . PHP_EOL .
+            'int main() {' . PHP_EOL .
+            '    cout << "Hello, world!" << endl;' . PHP_EOL .
+            '    return 0;' . PHP_EOL .
+            '}'
+        );
+        $manager->persist($language);
+        $manager->persist($snippet);
+
+        $manager->flush();
+    }
+}
