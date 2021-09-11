9b51a34558cc8c9b05e77037e5c174eaa3f0e41f
Piotr Sokołowski <dev@sokoloowski.pl>
2021-09-11 20:35:11 +0200

    Fixed migration to work with postgres

--- migrations/Version20210908112243.php
+++ migrations/Version20210908112243.php
@@ -20,10 +20,10 @@ final class Version20210908112243 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE language (id BLOB NOT NULL --(DC2Type:uuid)
+        $this->addSql('CREATE TABLE language (id uuid NOT NULL --(DC2Type:uuid)
         , name VARCHAR(200) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE snippet (id BLOB NOT NULL --(DC2Type:uuid)
-        , language_id BLOB NOT NULL --(DC2Type:uuid)
+        $this->addSql('CREATE TABLE snippet (id uuid NOT NULL --(DC2Type:uuid)
+        , language_id uuid NOT NULL --(DC2Type:uuid)
         , name VARCHAR(200) NOT NULL, code VARCHAR(1000) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_961C8CD582F1BAF4 ON snippet (language_id)');
     }
